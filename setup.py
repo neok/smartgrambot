825fd0d4e46da90a831cd432b05f8a4a8938b11f
neok <p.popelyshko@gmail.com>
2019-09-20 18:00:17 +0300

    Updated requirements and setup

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ from setuptools import setup
 
 import smartgrambot
 
-
+try:
+    abs_path = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(abs_path, 'requirements/main.txt')) as file:
+        for line in file:
+            required = [line.strip("\n")]
+except IOError:
+    pass
 
 setup(
     name="smartgrambot",
@@ -13,9 +19,10 @@ setup(
     author="Petro Popelyshko",
     long_description="Smart instagram bot...",
     author_email="p.popelyshko@gmail.com",
+    install_requires=required,
     license="GPLv3",
-    entry_points= {
-        "console_scripts" : [
+    entry_points={
+        "console_scripts": [
             "smartgrambot = smartgrambot.__main__:main"
         ]
     }
